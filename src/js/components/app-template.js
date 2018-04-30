05febc497156a83ffe55154a453af8d239aefd7b
martafrr <martafarreribas@gmail.com>
2018-04-30 11:37:36 +0200

    first commit

---
+++ src/js/components/app-template.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './header/app-header';
+
+
+export default (props) => {
+  return (
+    <div className="container">
+      <Header></Header>
+      { props.children }
+    </div>
+  )
+}
---
+++ src/js/components/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Catalog from './catalog/app-catalog';
+import Cart from './cart/app-cart';
+import CatalogDetail from './product/app-catalog-detail';
+import Template from './app-template';
+import { Router, Route, IndexRoute } from 'react-router';
+
+export default () => {
+  return (
+    <Router>
+      <Route path="/" component={Template}>
+        <IndexRoute component={Catalog} />
+        <Route path="cart" component={Cart} />
+        <Route path="item/:item" component={CatalogDetail} />
+      </Route>
+    </Router>
+  );
+}
